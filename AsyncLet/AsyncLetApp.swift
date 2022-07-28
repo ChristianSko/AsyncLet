6364d06ee23fbc66d687916426ec8fa5fd7618be
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-28 08:50:08 +0200

    Initial Commit

---
+++ AsyncLet/AsyncLetApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncLetApp.swift
+//  AsyncLet
+//
+//  Created by Skorobogatow, Christian on 28/7/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncLetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
